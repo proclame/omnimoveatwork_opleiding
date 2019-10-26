910b71b9005bd9f9ec8fbf7779803920b8fae1f6
Nick Mispoulier <nick@proclame.be>
2019-10-26 11:26:57 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  theme: {
+    extend: {
+        colors: {
+            'brand-teal': '#65BB98',
+        }
+    },
+    container: {
+      center: true,
+      padding: '2rem',
+    },
+  },
+  variants: {},
+  plugins: []
+}
