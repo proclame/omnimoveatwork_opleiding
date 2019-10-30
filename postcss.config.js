f3efe2c07c715211cb75ea264e4f1b39f52b7399
Nick Mispoulier <nick@proclame.be>
2019-10-30 23:33:51 +0100

    Updates

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,15 @@
 module.exports = {
   plugins: [
-    // ...
     require('tailwindcss'),
     require('autoprefixer'),
-    // ...
+    require('@fullhuman/postcss-purgecss')({
+        content: [
+        '*.html',
+        ],
+        defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+    }),
+    require('cssnano')({
+            preset: 'default',
+        }),
   ]
 }
